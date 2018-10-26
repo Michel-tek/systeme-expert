d47c7976c729b96f802ec4b52b4c6f26fda394f4
Daniel Campos do Nascimento <dcdn.96@gmail.com>
2018-10-26 21:22:46 +0200

    Plus d'éléments

--- Interface.py
+++ Interface.py
@@ -1,23 +1,45 @@
-import tkinter as Tk
+from tkinter.ttk import *
+from tkinter import IntVar, BooleanVar, N, S, W, E
 
-class Interface(Tk.Frame):
+class Interface(Frame):
 	def __init__(self, master=None):
 		## Initialisation de la fenêtre
-		Tk.Frame.__init__(self, master)
+		Frame.__init__(self, master)
 		self.grid()
 		self.master.title("Moteur 0+")
+		self.grid_configure(sticky=(N, S, E, W))
 
 		## Grandes divisions
+		self.divisions = Notebook(self)
+		self.divisions.grid(sticky=(N, S, E, W))
+		self.divisions.grid_anchor(anchor="center")
 
-		self.cadre_controle = Tk.Frame(self, bd=2).grid()
-		self.cadre_faits = Tk.Frame(self, bd=2).grid()
+		self.base = Frame(self.divisions)
+		self.divisions.add(self.base, text="Base")
 
-		## Option de chaînage avant ou arrière
-		self.chainage         = Tk.IntVar()
-		self.chainage_avant   = Tk.Radiobutton(self.cadre_controle, variable=self.chainage, value=0, text="Avant").grid()
-		self.chainage_arriere = Tk.Radiobutton(self.cadre_controle, variable=self.chainage, value=1, text="Arrière").grid()
-		#self.chainage_avant.grid()
-		#self.chainage_arriere.grid()
+		self.cas  = Frame(self.divisions)
+		self.divisions.add(self.cas, text="Cas")
+		self.cas.columnconfigure(0, weight=1)
+		self.cas.rowconfigure(0, weight=1)
+
+		self.cadre_faits    = Labelframe(self.cas, text="Faits")
+		self.cadre_faits.grid(column=0, sticky=(N, S, E, W))
+		self.cadre_controle = Labelframe(self.cas, text="Controle")
+		self.cadre_controle.grid(column=1, sticky=(N, S))
+
+		## Controle:
+		##    Option de chaînage avant ou arrière
+		self.chainage         = IntVar(0)
+		self.chainage_avant   = Radiobutton(self.cadre_controle, variable=self.chainage, value=0, text="Avant")
+		self.chainage_avant.grid(sticky=W)
+		self.chainage_arriere = Radiobutton(self.cadre_controle, variable=self.chainage, value=1, text="Arrière")
+		self.chainage_arriere.grid(sticky=W)
+
+		##    Lancer
+		self.lance  = BooleanVar
+		self.lancer = Button(self.cadre_controle, text="Lancer").grid(sticky=W)
+
+
 
 	def changement_chainage(self):
 		pass
