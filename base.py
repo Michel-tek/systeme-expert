4407797d884ce8b65eb85a855d2079c7c288108a
Daniel Campos do Nascimento <dcdn.96@gmail.com>
2018-10-26 14:45:45 +0200

    Ajout des autres modes d'opération

--- base.py
+++ base.py
@@ -7,9 +7,9 @@ from sys import argv, exit, stderr
 msg = stderr.write
 
 ## Usage
-usage = ( "Usage: {0} interactive\n"
-        + "     | {0} avant        <base> [ < <faits> ] [ > <conclusions> ])\n"
-        + "     | {0} arriere      <base> [ < <faits> ] [ > <conclusions> ])\n").format(argv[0])
+usage = ( "Usage: {0} interactive [ <base> ]\n"
+        + "     | {0} avant         <base> [ < <faits> ] [ > <conclusions> ])\n"
+        + "     | {0} arriere       <base> [ < <faits> ] [ > <conclusions> ])\n").format(argv[0])
 
 if len(argv) == 1:
 	msg(usage)
@@ -26,5 +26,21 @@ from Moteur import Moteur
 ## Triage du mode d'opération
 if argv[1] == "interactive":
 	from Interface import Interface
-	application = Interface()
+
+	try:
+		## Analyser la base de connaissances en entrée et
+		## initialiser l'application là-dessus
+		application = Interface()
+	except IndexError:
+		application = Interface()
+
 	application.mainloop()
+
+elif argv[1] in {"avant", "arriere"}:
+	from sys import stdin, stdout
+
+	## À compléter en fonction du modèle qui sera adopté
+
+else:
+	msg(usage)
+	exit(1)
