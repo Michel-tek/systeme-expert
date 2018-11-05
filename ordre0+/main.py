879ba386eaade8d126dc3ae9d775c6ba6b8460bc
Jykojo <cyril.grelier@outlook.fr>
2018-11-05 14:04:30 +0100

    le moteur fonctionne!!!!

--- ordre0+/main.py
+++ ordre0+/main.py
@@ -23,17 +23,18 @@ for i, f in enumerate(liste_des_faits):
     string += "\n\t\t" + str(i) + " - " + str(f)
 print(string)
 
+
 print("\n\nTEST CHAINAGE ARRIERE\n\n")
 
 liste_des_faits = recuperation_des_faits("faits_chien_arriere.xml")
 
-moteur.chainage_arriere(liste_des_faits, Predicat("il fait beau"))
+moteur.chainage_arriere(liste_des_faits, Predicat("le temps", "=", "soleil"))
 
 print("\n\nRESULTAT CHAINAGE ARRIERE\n\n")
 
 print(moteur)
 
-string = "\n\t*Liste des faits"
+string = "\n*Liste des faits"
 for i, f in enumerate(liste_des_faits):
-    string += "\n\t\t" + str(i) + " - " + str(f)
+    string += "\n\t" + str(i) + " - " + str(f)
 print(string)
